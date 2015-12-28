d24c233a5be2df24dce6309d639b65b2474a071c
Guoqing <zhaogq10@163.com>
2015-12-28 23:37:29 +0800

    using jbuilder for rendering articles json

---
+++ app/views/api/v1/articles/index.json.jbuilder
@@ -0,0 +1,9 @@
+json.articles do
+  json._links do
+    json.self do
+      json.href '/api/v1/articles'
+    end
+  end
+
+  json.articles @articles
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
