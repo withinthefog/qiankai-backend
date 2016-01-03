ca5031c17cc3916ce3bf232bd70daebbd08f33e0
Guoqing <zhaogq10@163.com>
2016-01-03 23:37:07 +0800

    create space model and admin page

---
+++ app/admin/create_space.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register CreateSpace do
+  menu parent:'众创空间'
+  permit_params :introduction, :policy, :support
+
+  index do
+    selectable_column
+    id_column
+    column :introduction
+    column :policy
+    column :support
+    column :created_at
+    actions
+  end
+
+  filter :introduction
+  filter :policy
+  filter :support
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :introduction
+      f.input :policy
+      f.input :support
+    end
+    f.actions
+  end
+
+end
--- app/models/create_space.rb
+++ app/models/create_space.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: create_spaces
+#
+#  id           :integer          not null, primary key
+#  introduction :text
+#  policy       :text
+#  support      :text
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class CreateSpace < ActiveRecord::Base
 end
