9fe2bd9cf74ed227b6b90e5dd01844b6c7ecc5ec
mandosir <624459434@qq.com>
2016-04-01 23:08:53 +0800

    add tourism model

---
+++ app/models/tourism.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: tourisms
+#
+#  id                          :integer          not null, primary key
+#  title                       :string(255)      景点名称
+#  description                 :string(255)      景点描述
+#  content                     :text             景点正文
+#  created_at                  :datetime
+#  updated_at                  :datetime
+#
+
+class Product < ActiveRecord::Base  
+  has_and_belongs_to_many :tourism_tags
+  has_many :attachments, as: :attachable
+  accepts_nested_attributes_for :attachments, allow_destroy: true
+end
