928d41cc3ef2e5c9a28707df3ed34018fa0e54f2
Guoqing <zhaogq10@163.com>
2016-04-12 00:44:16 +0800

    hot fix

--- app/controllers/api/v1/wechats_controller.rb
+++ app/controllers/api/v1/wechats_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::WechatsController < ApplicationController
   def sign_in
-    @consumer = Consumer.create(email: params['code']+'@wechat.com', password: 'zhiyecaoshou')
+    @consumer = Consumer.find_or_create_by(email: params['code']+'@wechat.com') do |consumer|
+      consumer.password = 'zhiyecaoshou'
+    end
 
     render :json => {
                :consumer => @consumer,
