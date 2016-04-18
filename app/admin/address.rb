ebd30db3b0b0ea7570d2b258d8082a17b7203b8e
Guoqing <zhaogq10@163.com>
2016-04-19 00:02:58 +0800

    Address admin page

--- app/admin/address.rb
+++ app/admin/address.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Address do
   menu parent:'订单管理'
+  index do
+    column :id
+    column :receiver
+    column :phone
+    column :address
+    column :city_name
+    actions
+  end
 end
