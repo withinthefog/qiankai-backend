34f9048abe88b2141a6b678ce8caebec40ea050e
Guoqing <zhaogq10@163.com>
2016-04-19 14:39:03 +0800

    i18n configuration

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Qiankai
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = false
+    config.i18n.locale = 'zh-CN'
     config.i18n.default_locale = 'zh-CN'
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
