d1eedcc1386394e80d6274f84bcd95909bd59fa0
Guoqing <zhaogq10@163.com>
2016-04-25 07:26:14 +0800

    update order

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -20,12 +20,8 @@ class Api::V1::TransactionsController < ApiController
                        transaction_type: params['type'],
                        amount: params['data']['object']['amount'])
 
-    if (params['data']['object']['paid'])
-      Order.find_by_sn(params['data']['object']['order_no']).update_attribute(state: '已支付')
-      return render json: {status: 200}
-    else
-      return render json: {status: 500}
-    end
+    Order.find_by_sn(params['data']['object']['order_no']).update_attribute(state: '已支付')
+    return render json: {status: 200}
 
   rescue Exception
     render json: {status: 500}
