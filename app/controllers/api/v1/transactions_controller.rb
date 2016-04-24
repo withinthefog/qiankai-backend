7a66c62d2946aa6af0d62c296ebca11bba8de506
Guoqing <zhaogq10@163.com>
2016-04-25 02:29:43 +0800

    paid callback

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -14,5 +14,20 @@ class Api::V1::TransactionsController < ApiController
 
   def create
     puts params
+    Transaction.create(pingpp_id: params[:id],
+                       order_id: params['data']['object']['order_no'],
+                       status: params['data']['object']['paid'],
+                       transaction_type: params['type'],
+                       amount: params['data']['object']['amount'])
+
+    if (params['data']['object']['paid'])
+      Order.find_by_sn(params['data']['object']['order_no']).update_attribute(state: '已支付')
+      return render json: {status: 200}
+    else
+      return render json: {status: 500}
+    end
+
+  rescue Exception
+    render json: {status: 500}
   end
 end
--- app/services/ping_pp_service.rb
+++ app/services/ping_pp_service.rb
@@ -2,7 +2,7 @@ class PingPPService
   def self.create_payment(order, client_ip)
     Pingpp::Charge.create(
         order_no: order.sn,
-        amount: order.total_price + order.ship_fee,
+        amount: (order.total_price + order.ship_fee) * 100,
         subject: order.consumer.openid,
         body: (order.line_items.map { |line_item| line_item.product.name }).to_json,
         channel: 'wx_pub',
