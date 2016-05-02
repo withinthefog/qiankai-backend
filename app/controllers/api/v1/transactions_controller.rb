9dee96e07cfabfdca8ae104f253ca46f94940d08
Guoqing <zhaogq10@163.com>
2016-05-02 11:02:31 +0800

    More info for payment

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::TransactionsController < ApiController
   before_action :authenticate_consumer!
 
   def new
-    if params[:sn].nil? || (order = Order.find_by_sn(params[:sn])).consumer != current_consumer || order.state == '已支付'
+    if params[:sn].nil? || (order = Order.find_by_sn(params[:sn])).try(:consumer) != current_consumer || order.state == '已支付'
       @error = 'Invalid payment request'
       return render :error
     end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,7 @@ class ApiController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found_exception
   rescue_from UnauthorizedException, with: :handle_unauthorized_exception
+  rescue_from Pingpp::InvalidRequestError, with: :handle_ping_pp_invalid_request
 
   private
   def handle_record_not_found_exception(exception)
@@ -13,5 +14,10 @@ class ApiController < ApplicationController
   def handle_unauthorized_exception(exception)
     logger.info("Not authorized to this operation: #{exception.message}")
     render json: {message: exception.message}.to_json, status: 401
+  end
+
+  def handle_ping_pp_invalid_request(exception)
+    logger.info("Invalid ping pp request: #{exception.message}")
+    render json: {message: exception.message}.to_json, status: 422
   end
 end
