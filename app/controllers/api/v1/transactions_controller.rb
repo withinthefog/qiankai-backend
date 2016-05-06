f15145c374d71401aac3048b92b3f98f3ace3830
Guoqing <zhaogq10@163.com>
2016-05-06 21:25:35 +0800

    remove token authentication for ping pp callback

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::TransactionsController < ApiController
-  before_action :authenticate_consumer_from_token!
-  before_action :authenticate_consumer!
+  before_action :authenticate_consumer_from_token!, only: [:new]
+  before_action :authenticate_consumer!, only: [:new]
 
   def new
     if params[:sn].nil? || (order = Order.find_by_sn(params[:sn])).try(:consumer) != current_consumer || order.state == '已支付'
