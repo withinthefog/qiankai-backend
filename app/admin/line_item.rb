17cca0ec34b1c677f40d9ab41ceecaf8c0852524
Guoqing <zhaogq10@163.com>
2016-05-09 21:22:36 +0800

    Manage line item

---
+++ app/admin/line_item.rb
@@ -0,0 +1,40 @@
+ActiveAdmin.register LineItem do
+  menu parent:'订单管理'
+  permit_params :state, :handle_state, :logistical, :logistical_number, :unit_price
+
+  index do
+    selectable_column
+    column :id
+    column :product do |line_item|
+      "商品ID：#{line_item.product_id}\n商品名称：#{line_item.try(:product).try(:name)}"
+    end
+    column :consumer do |line_item|
+      line_item.order.consumer.openid ? "微信用户：#{line_item.order.consumer.nickname}" : line_item.order.consumer.email if line_item.order.consumer.present?
+    end
+    column :address do |line_item|
+      link_to line_item.order.address.city_name, admin_address_path(line_item.order.address) if line_item.order.address.present?
+    end
+    column :state do |line_item|
+      line_item.order.state
+    end
+    column :handle_state
+    column :unit_price
+    column :quantity
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs "订单详情" do
+      f.input :handle_state, as: :select, collection: ['未处理', '正在处理', '已发货', '已完成']
+      f.input :logistical
+      f.input :logistical_number
+      f.input :unit_price
+    end
+    f.actions
+  end
+
+  filter :created_at
+  filter :state
+  filter :handle_state
+end
