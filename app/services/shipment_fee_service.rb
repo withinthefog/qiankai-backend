a5001509d622d7bc1c2b69769fe6ae69dfaf123d
Guoqing <zhaogq10@163.com>
2016-05-18 15:17:15 +0800

    Fix shipment fees

--- app/services/shipment_fee_service.rb
+++ app/services/shipment_fee_service.rb
@@ -1,7 +1,7 @@
 class ShipmentFeeService
   def self.calculate(address_id)
     address = Address.find(address_id)
-    shipment_fee = ShipmentFee.find_by_city_id(address.city_id) || ShipmentFee.find_by_province_id(address.province_id)
+    shipment_fee = ShipmentFee.find_by_city_id(address.city_id) || ShipmentFee.where(province_id: address.province_id, city_id: nil)[0]
     shipment_fee.try(:price) || 0
   end
 end
